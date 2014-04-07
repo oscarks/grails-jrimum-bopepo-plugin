b145b31dc3205e9f6c4fcc03b57a107a25c7c0d3
Oscar Konno Sampaio <oscar@acception.com.br>
2014-04-07 00:43:22 -0300

    PDF template configuration

--- src/groovy/com/mrkonno/plugin/jrimum/Boleto.groovy
+++ src/groovy/com/mrkonno/plugin/jrimum/Boleto.groovy
@@ -2,22 +2,35 @@ package com.mrkonno.plugin.jrimum
 
 class Boleto {
 	def boleto
+	def template
 	
 	Boleto(org.jrimum.bopepo.Boleto boleto) {
 		this.boleto=boleto
 	}
+	Boleto(org.jrimum.bopepo.Boleto boleto,String template) {
+		this.boleto=boleto
+		this.template=template
+	}
+	
+	def getBoletoView() {
+		def bview
+		if (template)
+			bview=new org.jrimum.bopepo.view.BoletoViewer(boleto,template)
+		else bview=new org.jrimum.bopepo.view.BoletoViewer(boleto)
+		bview
+	}
 	
 	byte[] getBytes() {
-		def bview=new org.jrimum.bopepo.view.BoletoViewer(boleto)
+		def bview=boletoView
 		bview.getPdfAsByteArray()
 	} 
 	
 	File pdf(String fn) {
-		def bview=new org.jrimum.bopepo.view.BoletoViewer(boleto)
+		def bview=boletoView
 		bview.getPdfAsFile(fn)
 	}
 	ByteArrayOutputStream getStream() {
-		def bview=new org.jrimum.bopepo.view.BoletoViewer(boleto)
+		def bview=boletoViewer
 		bview.getPdfAsStream()
 	}
 	
